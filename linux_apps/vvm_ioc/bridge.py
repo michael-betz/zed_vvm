043dbe52c1c8b9e4ac4ae42c938f68a1eeee8af4
Michael Betz <michibetz@gmail.com>
2020-07-25 12:08:53 -0700

    bridge.py: experimental anti-drift loop

--- linux_apps/vvm_ioc/bridge.py
+++ linux_apps/vvm_ioc/bridge.py
@@ -1,6 +1,10 @@
-
+import time
 import paho.mqtt.client as mqtt
-from epics import caput
+from epics import caput, caget
+
+LOOP_DT = 1000
+PV_PREFIX = 'GTL:VVM1:'
+N_AVG = 5
 
 
 def on_connect(client, userdata, flags, rc):
@@ -18,14 +22,12 @@ def on_message(client, userdata, msg):
     if msg.topic == 'vvm/results/mags':
         vals = getVals(msg.payload)
         for val, s in zip(vals, ('Ref', 'A', 'B', 'C')):
-            caput('GTL:VVM1:mag{:}'.format(s), val)
-            # print('iocvvm:mag{:}'.format(s), val)
+            caput(PV_PREFIX + 'mag{:}'.format(s), val)
 
     if msg.topic == 'vvm/results/phases':
         vals = getVals(msg.payload)
         for val, s in zip(vals, ('A', 'B', 'C')):
-            caput('GTL:VVM1:phase{:}'.format(s), val)
-            # print('iocvvm:phase{:}'.format(s), val)
+            caput(PV_PREFIX + 'phase{:}'.format(s), val)
 
 
 client = mqtt.Client()
@@ -33,5 +35,28 @@ client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("vvm0.dhcp")
+client.loop_start()
 
-client.loop_forever()
+meas = []
+phase_sp = 0
+int_err = 0
+while True:
+    isEnabled = caget(PV_PREFIX + 'phaseLock')
+
+    meas.append(caget(PV_PREFIX + 'phaseA'))
+    if len(meas) > N_AVG:
+        meas.pop(0)
+    avg_meas = sum(meas) / len(meas)
+
+    if isEnabled:
+        err = avg_meas - phase_sp
+        int_err += err
+        if int_err > 10:
+            int_err = 10
+        if int_err < -10:
+            int_err = -10
+        print(phase_sp, avg_meas, err, int_err)
+    else:
+        phase_sp = avg_meas
+
+    time.sleep(LOOP_DT - time.time() % LOOP_DT)
